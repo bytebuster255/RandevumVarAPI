a604521a9714f03a4beb27a9ce1221ea30a80720
bytebuster255 <byte.buster255@gmail.com>
2024-05-03 23:22:45 +0300

    Gelen veri Kontrolleri yapıldı

--- api.js
+++ api.js
@@ -5,7 +5,17 @@ const cors = require("cors");
 const Loginperson = require("./LoginPerson");
 
 const app = express();
-app.use(bodyParser.json());
+
+function customJsonMiddleware(req, res, next) {
+  bodyParser.json()(req, res, (err) => {
+    if (err) {
+      return res.status(400).send("Gelen veri JSON formatında değil");
+    }
+    next();
+  });
+}
+
+app.use(customJsonMiddleware);
 app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("X-Powered-By", "ByteBuster");
