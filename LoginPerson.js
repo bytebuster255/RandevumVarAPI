eee892404973c95714d1980a6a3f9240c75b9b3b
bytebuster255 <byte.buster255@gmail.com>
2024-05-03 23:38:49 +0300

    Değişken isimleri değişti ve gereksiz kısımlar silindi

--- LoginPerson.js
+++ LoginPerson.js
@@ -1,12 +1,11 @@
-const { User } = require("discord.js");
 const DatabaseManager = require("./DatabaseManager");
 const dbManager = require("./DatabaseManager");
 const tokenManager = require("./TokenManager");
 const bcrypt = require("bcrypt");
 
 async function Loginperson(req, res) {
-  const { LoginToken, Username } = req.body;
-  const requiredFields = ["LoginToken", "Username"];
+  const { Username, Password } = req.body;
+  const requiredFields = ["Username", "Password"];
 
   for (const field of requiredFields) {
     if (!req.body[field]) {
@@ -24,17 +23,16 @@ async function Loginperson(req, res) {
       return res.status(404).send("Kullanıcı bulunamadı");
     }
 
-    const matchCorrect = bcrypt.compareSync(LoginToken, user.HashedToken);
+    const matchCorrect = bcrypt.compareSync(Password, user.HashedToken);
 
     if (matchCorrect) {
-      return res
-        .status(200)
-        .json({
-          token: tokenManager.createToken({
-            userId: user.Id,
-            userName: user.Username,
-          }),
-        });
+      return res.status(200).json({
+        token: tokenManager.createToken({
+          userId: user.Id,
+          userName: user.Username,
+          userEmail: user.userEmail,
+        }),
+      });
     } else {
       return res.status(200).send("pass incorrect");
     }
